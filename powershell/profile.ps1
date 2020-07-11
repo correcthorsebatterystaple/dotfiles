3638e3cf9d5ec37b18b6c0983419247a9e71b25f
BSalim <burhan.salman@gmail.com>
2020-07-12 03:59:12 +0400

    Add printing ahead and behind commit count to prompt

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -10,30 +10,55 @@ function prompt() {
         $status = (git status -s)
         $totalChanged = ($status | Measure-Object -line).Lines
         
-        # Get new files in index
-        $totalStagedAdded = (git diff --name-only --cached --diff-filter=A | Measure-Object -line).Lines
-        # Get modified files in index
-        $totalStagedModified = (git diff --name-only --cached --diff-filter=M | Measure-Object -line).Lines
-        # Get deleted files in index
-        $totalStagedDeleted = (git diff --name-only --cached --diff-filter=D | Measure-Object -line).Lines
-        
         # Get new files not in index (Untracked files)
         $totalUnstagedAdded = (git ls-files --others --exclude-standard | Measure-Object -line).Lines
         # Get modified files not in index
         $totalUnstagedModified = (git diff --name-only --diff-filter=M | Measure-Object -line).Lines
         # Get deleted files not in index
         $totalUnstagedDeleted = (git diff --name-only --diff-filter=D | Measure-Object -line).Lines
+
+        # Get the name of the remote branch
+        $remote = (git rev-parse --abbrev-ref --symbolic-full-name '@{u}') 2> $null
         
         Write-Host " [" -nonewline
             if ($totalChanged -eq 0) { # if any changes not commited
                 Write-Host "$($branchName)" -ForegroundColor green -nonewline
             } else {
                 Write-Host "$($branchName)" -ForegroundColor yellow -nonewline
+
+                # Get new files in index
+                $totalStagedAdded = (git diff --name-only --cached --diff-filter=A | Measure-Object -line).Lines
+                # Get modified files in index
+                $totalStagedModified = (git diff --name-only --cached --diff-filter=M | Measure-Object -line).Lines
+                # Get deleted files in index
+                $totalStagedDeleted = (git diff --name-only --cached --diff-filter=D | Measure-Object -line).Lines
                 Write-Host " +$($totalStagedAdded) ~$($totalStagedModified) -$($totalStagedDeleted)" -ForegroundColor green -nonewline
+
                 Write-Host " | " -nonewline
+
+                # Get new files not in index (Untracked files)
+                $totalUnstagedAdded = (git ls-files --others --exclude-standard | Measure-Object -line).Lines
+                # Get modified files not in index
+                $totalUnstagedModified = (git diff --name-only --diff-filter=M | Measure-Object -line).Lines
+                # Get deleted files not in index
+                $totalUnstagedDeleted = (git diff --name-only --diff-filter=D | Measure-Object -line).Lines
                 Write-Host "+$($totalUnstagedAdded) ~$($totalUnstagedModified) -$($totalUnstagedDeleted)" -ForegroundColor red -nonewline
             }
         Write-Host "]" -nonewline
+
+        if ($remote) {
+            # Get how many commit the current branch is ahead of remote
+            $ahead = (git rev-list $branchName --not $remote --count)
+            # Get how many commit the current branch is behind remote
+            $behind = (git rev-list $remote --not $branchName --count)
+
+            if ($ahead -gt 0) {
+                Write-Host " ↑$($ahead)" -ForegroundColor green -nonewline
+            }
+            if ($behind -gt 0) {
+                Write-Host " ↓$($behind)" -ForegroundColor red -nonewline
+            }
+        }
         
     }
     "`n> "
